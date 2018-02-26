8c16bbb29b283dcbeef71eca47830aef065b8b8f
Raymond Knorr <rdk3@live.ca>
2018-02-26 11:14:25 -0600

    added an express server version of the API project

--- expressServer.js
+++ expressServer.js
@@ -1,23 +1,32 @@
-var express = require("express");
-var weather = require("./weatherDataCall.js");
-var app = express();
-app.get("/", function(req,res) {
-	console.log('hello again');
-	res.send("Hello world!");
-	res.end();
-});
+const https = require('https');
+const express = require('express');
 
-app.get('https://api.darksky.net/forecast/355c9574914c83b28623931d0c693e32/50.4452,104.6189?exclude=currently,minutely,daily,alerts,flags', function(req, res) {
-	console.log("this did something");
+var app = express();
+var data = '';
+
+https.get('https://api.darksky.net/forecast/355c9574914c83b28623931d0c693e32/50.4452,104.6189?exclude=currently,minutely,daily,alerts,flags',
+ 					(resp) => {
+
+	resp.on('data', (chunk) => {
+		data += chunk;
+	});
+
+	resp.on('end', () => {
+		data = JSON.parse(data);
+	})
+}).on("error", (err) => {
+	console.log("Error: " + err.message);
 });
 
 app.get('/weather', function(req, res) {
-	res.send('You asked to see the weather?');
-	var w = weather.weatherData();
-	res.send(`${w}`);
-	res.end();
+
+	res.send(data);
 });
 
+app.get('/graphs', function(req, res) {
+  res.sendFile('C:/Users/wowfactor-acer/Workspace/JS_Practice/Node_API_Access_Practice.html');
+});
 
-app.listen(1337);
-console.log('Server running at http://127.0.0.1:1337/');
+app.listen(1337, function () {
+	console.log('Server running at http://127.0.0.1:1337/');
+});
